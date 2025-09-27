d9dcf23e17ba7298049ab3ec9f9f984df0e5b30d
MayhemK <mayhemk57@gmail.com>
2025-09-27 04:45:58 +0000

    codespaces doesn't want me to see what I am doing

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-function test() {
-  console.log("hello world")
+function testRed() {
+  console.log("the world is Red")
+}
+function testYellow() {
+  console.log("the world is Yellow")
+}
+function testGreen() {
+  console.log("the world is Green")
+}
+function testBlue() {
+  console.log("the world is Blue")
 }
