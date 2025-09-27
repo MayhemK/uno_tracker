3e084df4eef4785938f757879b9d5adaa2d013fe
MayhemK <mayhemk57@gmail.com>
2025-09-27 15:18:54 +0000

    testing tailwind

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// @ts-nocheck
-const gameState = {
-  currentColor: 'red',
-  currentDirection: 1
-}
+// // @ts-nocheck
+// const gameState = {
+//   currentColor: 'red',
+//   currentDirection: 1
+// }
 
-const colorBlockElem = document.getElementById('color-block')
-const arrowBlockElem = document.getElementById('arrowBlock')
+// const colorBlockElem = document.getElementById('color-block')
+// const arrowBlockElem = document.getElementById('arrowBlock')
 
-/**
- * @param {string} newColor
- */
-function changeColor(newColor) {
-  gameState.currentColor = newColor
-  colorBlockElem.style.backgroundColor = newColor
+// /**
+//  * @param {string} newColor
+//  */
+// function changeColor(newColor) {
+//   gameState.currentColor = newColor
+//   colorBlockElem.style.backgroundColor = newColor
 
-  console.log(`the color is now ${newColor}`)
-}
+//   console.log(`the color is now ${newColor}`)
+// }
 
-function changeOrder() {
-  gameState.currentDirection ^= 1
-  arrowBlockElem.classList.add('bigArrow')
-  arrowBlockElem.style.transform = gameState.currentDirection === 1 ? 'scaleX(1)' : 'scaleX(-1)'
+// function changeOrder() {
+//   gameState.currentDirection ^= 1
+//   arrowBlockElem.classList.add('bigArrow')
+//   arrowBlockElem.style.transform = gameState.currentDirection === 1 ? 'scaleX(1)' : 'scaleX(-1)'
   
-  arrowBlockElem.style.transform = gameState.currentDirection === 1 ? 'scaleX(1)' : 'scaleX(-1)'
+//   arrowBlockElem.style.transform = gameState.currentDirection === 1 ? 'scaleX(1)' : 'scaleX(-1)'
 
-}
+// }
